8d5231a014e2a816c306dc5e62af6e70f9b1ce53
Johan Larsson <johan_gavle1@hotmail.com>
2016-01-07 21:22:14 +0100

    Initial

---
+++ ChildWindowBox/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace ChildWindowBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var window = new Window { Content = new ContentView(), DataContext = this.DataContext };
+            window.ShowDialog();
+        }
+    }
+}
---
+++ ChildWindowBox/ViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ChildWindowBox
+{
+    public class ViewModel
+    {
+        public string Name { get; } = "Foo";
+    }
+}
